935de5a0ab5f213178bf45bb756b057bcd178a20
Jean - Autofoz <jeanfelipe0610@gmail.com>
2023-08-24 15:11:46 -0300

    init do projeto com todas as dependencias

---
+++ src/test/java/com/orbitech/npvet/NpvetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orbitech.npvet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NpvetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
