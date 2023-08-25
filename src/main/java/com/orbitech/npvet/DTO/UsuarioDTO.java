9fa635e72ae4d06f7e26bb69056a60f6b2bb3204
vainicius <comercial.viniciusgabriel@gmail.com>
2023-08-25 13:27:22 -0300

    feat: UsuarioDTO concluído.

--- src/main/java/com/orbitech/npvet/DTO/UsuarioDTO.java
+++ src/main/java/com/orbitech/npvet/DTO/UsuarioDTO.java
@@ -1,4 +1,24 @@
 package com.orbitech.npvet.DTO;
 
+import com.orbitech.npvet.entity.TipoUsuario;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
 public class UsuarioDTO {
+
+    @NotNull(message = "Você precisa preencher um nome.")
+    private String nome;
+
+    @NotNull(message = "Você precisa preencher um CPF.")
+    private String cpf;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull(message = "Você precisar definir o tipo de Usuário entre: SECRETARIA, ADMINISTRADOR ou MEDICO.")
+    private TipoUsuario tipoUsuario;
+
+    @NotNull(message = "Você precisa definir um nome de usuário.")
+    private String username;
+
+    @NotNull(message = "Você precisa definir uma senha.")
+    private String senha;
 }
