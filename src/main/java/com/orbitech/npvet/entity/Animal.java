ffdff9306ab78d792f05e9b76cee3da3fd8934c9
Gustavo Piegat <gustavopiegat2004@gmail.com>
2023-08-25 19:50:19 -0300

    Fix Animal adicionado annotation de constructors

--- src/main/java/com/orbitech/npvet/entity/Animal.java
+++ src/main/java/com/orbitech/npvet/entity/Animal.java
@@ -1,15 +1,17 @@
 package com.orbitech.npvet.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "animal", schema = "public")
 public class Animal{
 
