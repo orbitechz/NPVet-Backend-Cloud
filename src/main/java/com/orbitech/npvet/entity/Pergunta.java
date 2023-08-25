14f4fd3639e8f0978e6dc2f97538c3702a631b73
Bouchra Akl <bushraakl1234@gmail.com>
2023-08-24 22:43:34 -0300

    entities base.

---
+++ src/main/java/com/orbitech/npvet/entity/Pergunta.java
@@ -0,0 +1,13 @@
+package com.orbitech.npvet.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "perguntas",schema = "public")
+@Getter
+@Setter
+public class Pergunta {
+}
