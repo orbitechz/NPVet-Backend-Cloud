d859fc2379dbc5e980503fadf7e534e46f5bc870
Gustavo Piegat <gustavopiegat2004@gmail.com>
2023-08-25 19:36:07 -0300

    Fix Animal adicionado @Column

--- src/main/java/com/orbitech/npvet/entity/Animal.java
+++ src/main/java/com/orbitech/npvet/entity/Animal.java
@@ -1,7 +1,10 @@
 package com.orbitech.npvet.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,6 +13,28 @@ import lombok.Setter;
 @Table(name = "animal", schema = "public")
 public class Animal{
 
+    @Column(nullable = false)
+    private String nome;
 
+    @Column(nullable = false)
+    private String especie;
+
+    @Column(nullable = false)
+    private Sexo sexo;
+
+    @Column(nullable = false)
+    private Integer idade;
+
+    @Column(nullable = false)
+    private Double peso;
+
+    @Column(nullable = false)
+    private String pelagem;
+
+    @Column(nullable = false)
+    private String procedencia;
+
+    @Column(nullable = false)
+    private Integer tutor_id;
 
 }
