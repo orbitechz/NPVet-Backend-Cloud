afabb9b07518d01cd6ff130ed4273d1992db20a9
Bouchra Akl <bushraakl1234@gmail.com>
2023-08-26 13:30:06 -0300

    fix: Constructors adicionados.

--- src/main/java/com/orbitech/npvet/Entity/AnamnesePergunta.java
+++ src/main/java/com/orbitech/npvet/Entity/AnamnesePergunta.java
@@ -2,13 +2,17 @@ package com.orbitech.npvet.Entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "anamnese-pergunta",schema = "public")
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AnamnesePergunta extends AbstractEntity {
     @ManyToOne
     @JoinColumn(name = "id_anamnese",nullable = false)
