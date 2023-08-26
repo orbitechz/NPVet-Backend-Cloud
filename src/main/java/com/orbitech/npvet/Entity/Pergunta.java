c1ed140d37ed933f3702afcace43e17c783540a8
Bouchra Akl <bushraakl1234@gmail.com>
2023-08-25 22:49:05 -0300

    feat: entidade pergunta criada .

--- src/main/java/com/orbitech/npvet/Entity/Pergunta.java
+++ src/main/java/com/orbitech/npvet/Entity/Pergunta.java
@@ -1,13 +1,26 @@
 package com.orbitech.npvet.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "perguntas",schema = "public")
-@Getter
-@Setter
-public class Pergunta {
+@Getter @Setter
+public class Pergunta extends AbstractEntity{
+
+    @OneToMany(mappedBy = "pergunta")
+    @JsonIgnoreProperties("pergunta")
+    private List<AnamnesePergunta> anamnesePerguntas = new ArrayList<>();
+
+    @Column(nullable = false,unique = true)
+    private String enunciado;
+
 }
