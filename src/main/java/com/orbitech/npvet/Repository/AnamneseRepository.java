77d94a9cecd9d0a67c4f5a075b57160b77411d86
Bouchra Akl <bushraakl1234@gmail.com>
2023-08-29 10:42:03 -0300

    feat: criação dos packages e base da anamnese.

---
+++ src/main/java/com/orbitech/npvet/Repository/AnamneseRepository.java
@@ -0,0 +1,7 @@
+package com.orbitech.npvet.Repository;
+
+import com.orbitech.npvet.Entity.Anamnese;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnamneseRepository extends JpaRepository<Anamnese,Long> {
+}
---
+++ src/main/java/com/orbitech/npvet/Service/AnamneseService.java
@@ -0,0 +1,6 @@
+package com.orbitech.npvet.Service;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AnamneseService {
+}
