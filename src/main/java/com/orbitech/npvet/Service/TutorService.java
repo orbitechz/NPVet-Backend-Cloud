c3da5f4e28b7dc2c20fb65eceb619f292011e175
Jean - Autofoz <jeanfelipe0610@gmail.com>
2023-09-01 16:16:13 -0300

    feat: init da Service de Tutor

---
+++ src/main/java/com/orbitech/npvet/Service/TutorService.java
@@ -0,0 +1,9 @@
+package com.orbitech.npvet.Service;
+
+import com.orbitech.npvet.Repository.TutorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TutorService {
+}
