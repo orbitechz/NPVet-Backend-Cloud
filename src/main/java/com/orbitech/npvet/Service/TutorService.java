cb6bed402f98e805999b787b2ebd36bffc5f71e3
Jean - Autofoz <jeanfelipe0610@gmail.com>
2023-09-01 16:20:38 -0300

    feat: Criado toTutorDTO e toTutor na Service de Tutor

--- src/main/java/com/orbitech/npvet/Service/TutorService.java
+++ src/main/java/com/orbitech/npvet/Service/TutorService.java
@@ -1,9 +1,23 @@
 package com.orbitech.npvet.Service;
 
+import com.orbitech.npvet.DTO.TutorDTO;
+import com.orbitech.npvet.Entity.Tutor;
 import com.orbitech.npvet.Repository.TutorRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TutorService {
+    @Autowired
+    private TutorRepository repository;
+    private ModelMapper mapper;
+
+    public TutorDTO toTutorDTO(Tutor tutor){
+        return mapper.map(tutor, TutorDTO.class);
+    }
+
+    public Tutor toTutor(TutorDTO tutorDTO){
+        return mapper.map(tutorDTO, Tutor.class);
+    }
 }
