f9c8ea4e35a0f2bd9e83413e1f44126771016226
Jean - Autofoz <jeanfelipe0610@gmail.com>
2023-09-01 17:31:16 -0300

    feat: rotas de getById e post de Tutor concluidas

--- src/main/java/com/orbitech/npvet/Controller/TutorController.java
+++ src/main/java/com/orbitech/npvet/Controller/TutorController.java
@@ -1,9 +1,11 @@
 package com.orbitech.npvet.Controller;
 
+import com.orbitech.npvet.DTO.TutorDTO;
 import com.orbitech.npvet.Service.TutorService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/tutor")
@@ -11,5 +13,13 @@ public class TutorController {
     @Autowired
     private TutorService service;
 
+    @GetMapping("/{id}")
+    public ResponseEntity<TutorDTO> getById(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(service.getById(id));
+    }
 
+    @PostMapping("/post")
+    public ResponseEntity<TutorDTO> create(@RequestBody @Validated TutorDTO tutorDTO){
+        return ResponseEntity.ok(service.create(tutorDTO));
+    }
 }
