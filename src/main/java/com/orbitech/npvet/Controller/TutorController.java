dfc3cd2d1bda8b5cfcbec0c304f4541dd3aa3952
Jean - Autofoz <jeanfelipe0610@gmail.com>
2023-09-01 18:47:55 -0300

    feat: feito rotas CRUD de tutor

--- src/main/java/com/orbitech/npvet/Controller/TutorController.java
+++ src/main/java/com/orbitech/npvet/Controller/TutorController.java
@@ -7,19 +7,40 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/tutor")
 public class TutorController {
     @Autowired
     private TutorService service;
-
-    @GetMapping("/{id}")
+    @GetMapping("/id/{id}")
     public ResponseEntity<TutorDTO> getById(@PathVariable("id") Long id) {
         return ResponseEntity.ok(service.getById(id));
     }
-
+    @GetMapping("/nome/{nome}")
+    public ResponseEntity<List<TutorDTO>> getByNome(@PathVariable("nome") String nome) {
+        return ResponseEntity.ok(service.getAllByNome(nome));
+    }
+    @GetMapping("/cpf/{cpf}")
+    public ResponseEntity<TutorDTO> getByCpf(@PathVariable("cpf") String cpf) {
+        return ResponseEntity.ok(service.getByCpf(cpf));
+    }
+    @GetMapping("/rg/{rg}")
+    public ResponseEntity<TutorDTO> getByRg(@PathVariable("rg") String rg) {
+        return ResponseEntity.ok(service.getByRg(rg));
+    }
     @PostMapping("/post")
     public ResponseEntity<TutorDTO> create(@RequestBody @Validated TutorDTO tutorDTO){
         return ResponseEntity.ok(service.create(tutorDTO));
+    }
+    @PostMapping("/update/{id}")
+    public ResponseEntity<TutorDTO> update(@PathVariable("id") Long id, @RequestBody @Validated TutorDTO tutorDTO){
+        return ResponseEntity.ok(service.update(id, tutorDTO));
+    }
+    @PostMapping("/delete/{id}")
+    public ResponseEntity<String> delete(@PathVariable("id") Long id){
+        service.delete(id);
+        return ResponseEntity.ok(String.format("Tutor %s desativado com sucesso!", id));
     }
 }
