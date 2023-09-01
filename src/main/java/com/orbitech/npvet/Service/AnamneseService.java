2e5fa4f92b4c3ee5386d648808f858aec14d9de7
Bouchra Akl <bushraakl1234@gmail.com>
2023-09-01 19:24:46 -0300

    feat: GET methods - service implementados.

--- src/main/java/com/orbitech/npvet/Service/AnamneseService.java
+++ src/main/java/com/orbitech/npvet/Service/AnamneseService.java
@@ -7,7 +7,9 @@ import com.orbitech.npvet.Repository.AnamneseRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,18 +25,47 @@ public class AnamneseService {
         this.modelMapper = modelMapper;
     }
 
-    public AnamneseDTO getById(Long id) {return null;}
+    public AnamneseDTO getById(Long id) {
+        Anamnese anamnese = anamneseRepository.findById(id).orElse(null);
+        Assert.notNull(anamnese, String.format("O ID = %s solicitado não foi encontrado no banco de dados.", id));
+        return toAnamneseDTO(anamnese);
+    }
 
     public List<AnamneseDTO> getAll() {
-        return null;
+        List<AnamneseDTO> anamneseDTOs = anamneseRepository.findAll()
+                .stream()
+                .map(this::toAnamneseDTO)
+                .toList();
+
+        if (anamneseDTOs.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        return anamneseDTOs;
     }
 
     public List<AnamneseDTO> getByTutorCpf(String cpf) {
-        return null;
+        List<AnamneseDTO> anamneseDTOs = anamneseRepository.findByTutorCpf()
+                .stream()
+                .map(this::toAnamneseDTO)
+                .toList();
+
+        Assert.isTrue(!anamneseDTOs.isEmpty(),
+                String.format("Nenhuma anamnese encontrada para o tutor com CPF: %s", cpf));
+
+        return anamneseDTOs;
     }
 
     public List<AnamneseDTO> getByTutorCpfAndAnimal(String cpf) {
-        return null;
+        List<AnamneseDTO> anamneseDTOs = anamneseRepository.findByTutorCpfAndAnimal()
+                .stream()
+                .map(this::toAnamneseDTO)
+                .toList();
+
+        Assert.isTrue(!anamneseDTOs.isEmpty(),
+                String.format("Nenhuma anamnese encontrada para o animal do tutor com CPF: %s", cpf));
+
+        return anamneseDTOs;
     }
 
     public Anamnese create(AnamneseDTO anamneseDTO) {
