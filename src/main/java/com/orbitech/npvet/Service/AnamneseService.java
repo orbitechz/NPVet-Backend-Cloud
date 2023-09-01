6571acf26f332580fef7f70fff827b2f5e2012b0
Bouchra Akl <bushraakl1234@gmail.com>
2023-09-01 13:02:14 -0300

    feat: AnamneseService method signature criados.

--- src/main/java/com/orbitech/npvet/Service/AnamneseService.java
+++ src/main/java/com/orbitech/npvet/Service/AnamneseService.java
@@ -1,6 +1,39 @@
 package com.orbitech.npvet.Service;
+import com.orbitech.npvet.DTO.AnamneseDTO;
+import com.orbitech.npvet.Entity.Anamnese;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AnamneseService {
+
+    public AnamneseDTO getById(Long id) {
+        return null;
+    }
+
+    public List<AnamneseDTO> getAll() {
+        return null;
+    }
+
+    public List<AnamneseDTO> getByTutorCpf(String cpf) {
+        return null;
+    }
+
+    public List<AnamneseDTO> getByTutorCpfAndAnimal(String cpf) {
+        return null;
+    }
+
+    public Anamnese create(AnamneseDTO anamneseDTO) {
+        return null;
+    }
+
+    public Anamnese update(Long id, AnamneseDTO anamneseDTO) {
+        return null;
+    }
+
+    public boolean delete(Long id) {
+        return false;
+    }
 }
