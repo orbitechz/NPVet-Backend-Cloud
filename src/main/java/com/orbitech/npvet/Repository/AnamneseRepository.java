0301792bb78c28821c84ceedcb0bc77884b035f8
Bouchra Akl <bushraakl1234@gmail.com>
2023-09-01 19:25:22 -0300

    feat: Inicial querys adicionadas.

--- src/main/java/com/orbitech/npvet/Repository/AnamneseRepository.java
+++ src/main/java/com/orbitech/npvet/Repository/AnamneseRepository.java
@@ -3,5 +3,10 @@ package com.orbitech.npvet.Repository;
 import com.orbitech.npvet.Entity.Anamnese;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
+import java.util.List;
+
 public interface AnamneseRepository extends JpaRepository<Anamnese,Long> {
+    List<Anamnese> findByTutorCpf();
+    List<Anamnese> findByTutorCpfAndAnimal();
 }
