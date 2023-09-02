3884d232b91b84ac78d47416b3fb8b6fb1bdd9b4
Bouchra Akl <bushraakl1234@gmail.com>
2023-09-02 17:13:27 -0300

    feat: service implementada.

--- src/main/java/com/orbitech/npvet/Service/PerguntaService.java
+++ src/main/java/com/orbitech/npvet/Service/PerguntaService.java
@@ -1,15 +1,15 @@
 package com.orbitech.npvet.Service;
 
-import com.orbitech.npvet.DTO.AnamneseDTO;
 import com.orbitech.npvet.DTO.PerguntaDTO;
-import com.orbitech.npvet.Entity.Anamnese;
 import com.orbitech.npvet.Entity.Pergunta;
 import com.orbitech.npvet.Repository.PerguntaRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 
+import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
 
@@ -45,21 +45,42 @@ public class PerguntaService {
     }
 
     public Pergunta create(PerguntaDTO perguntaDTO) {
-        return null;
+
+        if (perguntaRepository.existsByEnunciado(perguntaDTO.getEnunciado())) {
+            throw new DataIntegrityViolationException("O enunciado deve ser único.");
+        }
+        return perguntaRepository.save(toPergunta(perguntaDTO));
     }
 
     public Pergunta update(Long id, PerguntaDTO perguntaDTO) {
-        return null;
+
+        Pergunta existingPergunta = perguntaRepository.findById(id).orElse(null);
+        Assert.notNull(existingPergunta, String.format("O ID = %s solicitado não foi encontrado no banco de dados.", id));
+
+        if (!id.equals(perguntaDTO.getId())) {
+            throw new IllegalArgumentException("O ID na URL não corresponde ao ID no corpo da requisição.");
+        }
+
+        String enunciadoAtualizado = perguntaDTO.getEnunciado();
+        Pergunta existenteComMesmoEnunciado = perguntaRepository.findByEnunciado(enunciadoAtualizado);
+        if (existenteComMesmoEnunciado != null && !existenteComMesmoEnunciado.getId().equals(id)) {
+            throw new IllegalArgumentException("O enunciado deve ser único, e o enunciado fornecido já existe.");
+        }
+
+        return perguntaRepository.save(toPergunta(perguntaDTO));
     }
 
     public void delete(Long id) {
+        PerguntaDTO perguntaDTO = getById(id);
+        perguntaDTO.setDeletedAt(LocalDateTime.now());
+        perguntaRepository.save(toPergunta(perguntaDTO));
     }
 
-    public PerguntaDTO toPerguntaDTO(Pergunta pergunta){
+    public PerguntaDTO toPerguntaDTO(Pergunta pergunta) {
         return modelMapper.map(pergunta, PerguntaDTO.class);
     }
 
-    public Pergunta toPergunta(PerguntaDTO perguntaDTO){
+    public Pergunta toPergunta(PerguntaDTO perguntaDTO) {
         return modelMapper.map(perguntaDTO, Pergunta.class);
     }
 }
