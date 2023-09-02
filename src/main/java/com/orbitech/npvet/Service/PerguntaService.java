69bb28e86a05646d8a682059b2c9fe7ff8b85d23
Bouchra Akl <bushraakl1234@gmail.com>
2023-09-02 16:44:46 -0300

    feat: PerguntaService modelMapper e autowired adicionados.

--- src/main/java/com/orbitech/npvet/Service/PerguntaService.java
+++ src/main/java/com/orbitech/npvet/Service/PerguntaService.java
@@ -1,13 +1,29 @@
 package com.orbitech.npvet.Service;
 
+import com.orbitech.npvet.DTO.AnamneseDTO;
 import com.orbitech.npvet.DTO.PerguntaDTO;
+import com.orbitech.npvet.Entity.Anamnese;
 import com.orbitech.npvet.Entity.Pergunta;
+import com.orbitech.npvet.Repository.AnamneseRepository;
+import com.orbitech.npvet.Repository.PerguntaRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class PerguntaService {
+
+    private final PerguntaRepository perguntaRepository;
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public PerguntaService(PerguntaRepository perguntaRepository, ModelMapper modelMapper) {
+        this.perguntaRepository = perguntaRepository;
+        this.modelMapper = modelMapper;
+    }
+
     public PerguntaDTO getById(Long id) {
         return null;
     }
@@ -25,5 +41,13 @@ public class PerguntaService {
     }
 
     public void delete(Long id) {
+    }
+
+    public PerguntaDTO toPerguntaDTO(Pergunta pergunta){
+        return modelMapper.map(pergunta, PerguntaDTO.class);
+    }
+
+    public Pergunta toPergunta(PerguntaDTO perguntaDTO){
+        return modelMapper.map(perguntaDTO, Pergunta.class);
     }
 }
