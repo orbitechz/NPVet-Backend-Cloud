2ecdc2607ef93e20d8dfb10dabe7b26277b0eead
Bouchra Akl <bushraakl1234@gmail.com>
2023-09-02 13:02:04 -0300

    feat: PerguntaController http requests adicionados.

--- src/main/java/com/orbitech/npvet/Controller/PerguntaController.java
+++ src/main/java/com/orbitech/npvet/Controller/PerguntaController.java
@@ -1,9 +1,16 @@
 package com.orbitech.npvet.Controller;
 
+import com.orbitech.npvet.DTO.AnamneseDTO;
+import com.orbitech.npvet.DTO.PerguntaDTO;
+import com.orbitech.npvet.Entity.Anamnese;
+import com.orbitech.npvet.Entity.Pergunta;
 import com.orbitech.npvet.Service.PerguntaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/pergunta")
@@ -11,5 +18,45 @@ public class PerguntaController {
 
     @Autowired
     public PerguntaService perguntaService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PerguntaDTO> getById(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(perguntaService.getById(id));
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<PerguntaDTO>> getAll() {
+        return ResponseEntity.ok(perguntaService.getAll());
+    }
+
+    @PostMapping("/post")
+    public ResponseEntity<String> create(@RequestBody @Validated PerguntaDTO perguntaDTO) {
+        try {
+            Pergunta pergunta = perguntaService.create(perguntaDTO);
+            return ResponseEntity.ok("O registro da pergunta foi realizado com sucesso.");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest()
+                    .body("Ocorreu um erro durante o cadastro: " + e.getMessage());
+        }
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<String> update(@PathVariable Long id,
+                                         @RequestBody @Validated PerguntaDTO perguntaDTO) {
+        try {
+            Pergunta pergunta = perguntaService.update(id, perguntaDTO);
+            return ResponseEntity.ok("O registro da pergunta foi atualizado com sucesso.");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest()
+                    .body("Ocorreu um erro durante a atualização: " + e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> delete(@PathVariable Long id) {
+        this.perguntaService.delete(id);
+        return ResponseEntity.ok().body("Pergunta excluída com sucesso!");
+    }
+
 
 }
