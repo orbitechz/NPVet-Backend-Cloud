a8ae60d7be123cf06c8ede8fbd7dcf7b6a57667c
Bouchra Akl <bushraakl1234@gmail.com>
2023-09-02 13:02:43 -0300

    feat: PerguntaService methods inicializadas.

--- src/main/java/com/orbitech/npvet/Service/PerguntaService.java
+++ src/main/java/com/orbitech/npvet/Service/PerguntaService.java
@@ -1,7 +1,29 @@
 package com.orbitech.npvet.Service;
 
+import com.orbitech.npvet.DTO.PerguntaDTO;
+import com.orbitech.npvet.Entity.Pergunta;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PerguntaService {
+    public PerguntaDTO getById(Long id) {
+        return null;
+    }
+
+    public List<PerguntaDTO> getAll() {
+        return null;
+    }
+
+    public Pergunta create(PerguntaDTO perguntaDTO) {
+        return null;
+    }
+
+    public Pergunta update(Long id, PerguntaDTO perguntaDTO) {
+        return null;
+    }
+
+    public void delete(Long id) {
+    }
 }
