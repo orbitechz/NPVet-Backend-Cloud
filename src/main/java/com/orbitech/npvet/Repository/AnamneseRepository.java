c746c0dd94ff8004e4947b22d599eb0d1541ba47
Bouchra Akl <bushraakl1234@gmail.com>
2023-09-01 23:36:12 -0300

    feat : @Querys implementadas.

--- src/main/java/com/orbitech/npvet/Repository/AnamneseRepository.java
+++ src/main/java/com/orbitech/npvet/Repository/AnamneseRepository.java
@@ -2,11 +2,17 @@ package com.orbitech.npvet.Repository;
 
 import com.orbitech.npvet.Entity.Anamnese;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Collection;
 import java.util.List;
 
 public interface AnamneseRepository extends JpaRepository<Anamnese,Long> {
-    List<Anamnese> findByTutorCpf();
-    List<Anamnese> findByTutorCpfAndAnimal();
+
+    @Query("SELECT a FROM Anamnese a JOIN a.tutor t WHERE t.cpf = :cpf")
+    List<Anamnese> findByTutorCpf(@Param("cpf") String cpf);
+
+    @Query("SELECT a FROM Anamnese a JOIN a.tutor t JOIN a.animal an WHERE t.cpf = :cpf AND an.nome = :nome")
+    List<Anamnese> findByTutorCpfAndAnimal(@Param("cpf") String cpf, @Param("nome") String nome);
 }
