8c29af9638f9c64eea4402aaf564d72c44ea3789
Bouchra Akl <bushraakl1234@gmail.com>
2023-09-03 10:56:08 -0300

    feat : AnamnesePerguntaRepository criada.

---
+++ src/main/java/com/orbitech/npvet/Repository/AnamnesePerguntaRepository.java
@@ -0,0 +1,7 @@
+package com.orbitech.npvet.Repository;
+
+import com.orbitech.npvet.Entity.AnamnesePergunta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnamnesePerguntaRepository extends JpaRepository<AnamnesePergunta,Long> {
+}
