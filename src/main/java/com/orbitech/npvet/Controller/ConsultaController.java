d55f91b3428563c6595b1a27c9fa54e476c7e619
vainicius <comercial.viniciusgabriel@gmail.com>
2023-09-03 19:17:40 -0300

    feat: UsuarioController, Service, Repository - getById() ok

---
+++ src/main/java/com/orbitech/npvet/Controller/ConsultaController.java
@@ -0,0 +1,25 @@
+package com.orbitech.npvet.Controller;
+
+import com.orbitech.npvet.DTO.ConsultaDTO;
+import com.orbitech.npvet.Service.ConsultaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/consulta")
+public class ConsultaController {
+    @Autowired
+    private ConsultaService service;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ConsultaDTO>getById(@PathVariable("id")final long id) throws Exception {
+        return ResponseEntity.ok(service.getById(id));
+    }
+
+
+
+}
---
+++ src/main/java/com/orbitech/npvet/Service/ConsultaService.java
@@ -0,0 +1,28 @@
+package com.orbitech.npvet.Service;
+import com.orbitech.npvet.DTO.ConsultaDTO;
+import com.orbitech.npvet.Entity.Consulta;
+import com.orbitech.npvet.Repository.ConsultaRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConsultaService {
+    @Autowired
+    private ConsultaRepository repository;
+
+    private final ModelMapper mapper = new ModelMapper();
+
+    public ConsultaDTO toConsultaDTO(Consulta consultaEntidade){
+        return mapper.map(consultaEntidade, ConsultaDTO.class);
+    }
+
+    public Consulta toConsultaEntidade(ConsultaDTO consultaDTO){
+        return mapper.map(consultaDTO, Consulta.class);
+    }
+
+    public ConsultaDTO getById (long id) throws Exception{
+        return toConsultaDTO(repository.findById(id).orElseThrow(()-> new Exception(String.format("Usuário com o id [%s] não localizado.",id))));
+    }
+
+}
