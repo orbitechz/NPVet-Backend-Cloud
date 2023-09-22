3b10b9104be113a3eb55f32a581c5c7313f94133
Bouchra Akl <bushraakl1234@gmail.com>
2023-09-22 14:09:18 -0300

    feat : Repository da AnamneseHistorico criada.

---
+++ src/main/java/com/orbitech/npvet/Repository/AnamneseHistoricoRepository.java
@@ -0,0 +1,7 @@
+package com.orbitech.npvet.Repository;
+
+import com.orbitech.npvet.Entity.AnamneseHistorico;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnamneseHistoricoRepository extends JpaRepository<AnamneseHistorico,Long> {
+}
