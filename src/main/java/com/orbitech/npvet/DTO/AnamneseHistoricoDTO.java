f6d2a7b05b28bf22893bbe1172fcac0b7a965ba4
Bouchra Akl <bushraakl1234@gmail.com>
2023-09-23 10:33:54 -0300

    fix : adicionando JSON properties.

--- src/main/java/com/orbitech/npvet/DTO/AnamneseHistoricoDTO.java
+++ src/main/java/com/orbitech/npvet/DTO/AnamneseHistoricoDTO.java
@@ -1,5 +1,9 @@
 package com.orbitech.npvet.DTO;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIdentityReference;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
@@ -11,8 +15,12 @@ import java.time.LocalDate;
 @Getter
 @Setter
 public class AnamneseHistoricoDTO extends AbstractEntityDTO {
+
+    @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+    @JsonIdentityReference(alwaysAsId = true)
+    @JsonIgnoreProperties("historicoProgressoMedico")
+    private AnamneseDTO anamnese;
     @NotBlank(message = "O progresso médico não pode estar em branco.")
-    @Size(max = 255, message = "O progresso médico deve ter no máximo 255 caracteres.")
     private String progressoMedico;
     private LocalDate dataAtualizacao;
 }
--- src/main/java/com/orbitech/npvet/Entity/AnamneseHistorico.java
+++ src/main/java/com/orbitech/npvet/Entity/AnamneseHistorico.java
@@ -1,5 +1,9 @@
 package com.orbitech.npvet.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIdentityReference;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -18,6 +22,9 @@ public class AnamneseHistorico extends AbstractEntity{
 
     @ManyToOne
     @JoinColumn(name = "anamnese_id", nullable = false)
+    @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+    @JsonIdentityReference(alwaysAsId = true)
+    @JsonIgnoreProperties("historicoProgressoMedico")
     private Anamnese anamnese;
 
     @Column(name = "progresso_medico")
