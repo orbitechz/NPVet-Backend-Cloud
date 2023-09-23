ef5341423baa0fcc0aa600c519c1ec019ccc7897
Bouchra Akl <bushraakl1234@gmail.com>
2023-09-23 10:34:22 -0300

    feat : Query AnamneseHistoricoRepository.existsByProgressoMedico adicionada.

--- src/main/java/com/orbitech/npvet/Repository/AnamneseHistoricoRepository.java
+++ src/main/java/com/orbitech/npvet/Repository/AnamneseHistoricoRepository.java
@@ -2,6 +2,10 @@ package com.orbitech.npvet.Repository;
 
 import com.orbitech.npvet.Entity.AnamneseHistorico;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface AnamneseHistoricoRepository extends JpaRepository<AnamneseHistorico,Long> {
+    @Query("SELECT CASE WHEN COUNT(a) > 0 THEN true ELSE false END FROM AnamneseHistorico a WHERE a.progressoMedico = :progressoMedico")
+    boolean existsByProgressoMedico(@Param("progressoMedico") String progressoMedico);
 }
