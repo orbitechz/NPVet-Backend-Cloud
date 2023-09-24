8c4a24057b2181be146a9572e837a11c06c5857c
vainicius <comercial.viniciusgabriel@gmail.com>
2023-09-24 20:19:19 -0300

    fix: Adicionando exameFísico ao DTO e corrigindo Status.

--- src/main/java/com/orbitech/npvet/DTO/ConsultaDTO.java
+++ src/main/java/com/orbitech/npvet/DTO/ConsultaDTO.java
@@ -1,10 +1,6 @@
 package com.orbitech.npvet.DTO;
 
-import com.orbitech.npvet.Entity.Anamnese;
-import com.orbitech.npvet.Entity.Animal;
-import com.orbitech.npvet.Entity.Tutor;
-import com.orbitech.npvet.Entity.Usuario;
-
+import com.orbitech.npvet.Entity.Status;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -24,11 +20,12 @@ public class ConsultaDTO extends AbstractEntityDTO {
     @NotNull(message = "Você precisa selecionar um tutor.") //TODO: Decidir se deixamos o id do tutor na consulta, ou apenas na anamnese.
     private TutorDTO tutor;
     private List<AnamneseDTO> anamnese;
+    private List<ExameFisicoDTO>examesFisicos;
     @NotNull(message = "Informe a data.")
     private LocalDateTime data;
-    @NotNull (message = "Você precisar informar o status.") //TODO: Verificar se isso não é um Enum
+    @NotNull (message = "Você precisar informar o status.")
     @Size(max = 15)
-    private String status;
+    private Status status;
     @NotNull(message = "Você precisa informar o Médico Veterinário!")
     private UsuarioDTO veterinario;
 }
