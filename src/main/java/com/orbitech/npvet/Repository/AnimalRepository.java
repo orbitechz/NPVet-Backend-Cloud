ae83117fe9c526e1cb5257dd38708b3e2a6d07cb
Bouchra Akl <bushraakl1234@gmail.com>
2023-09-27 19:59:48 -0300

    merge conflict resolved.

--- src/main/java/com/orbitech/npvet/Repository/AnimalRepository.java
+++ src/main/java/com/orbitech/npvet/Repository/AnimalRepository.java
@@ -1,7 +1,6 @@
 package com.orbitech.npvet.Repository;
 
 import com.orbitech.npvet.Entity.Animal;
-import com.orbitech.npvet.Entity.Tutor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
