3a40c7e3fcd6a0bfc0807a19fe4a018d145e624d
Bouchra Akl <bushraakl1234@gmail.com>
2023-09-29 20:49:26 -0300

    test : create + update + delete integration test implemented.

--- src/test/java/com/orbitech/npvet/ControllerTest/AnamneseControllerTest.java
+++ src/test/java/com/orbitech/npvet/ControllerTest/AnamneseControllerTest.java
@@ -1,21 +1,17 @@
 package com.orbitech.npvet.ControllerTest;
 
 import com.orbitech.npvet.controller.AnamneseController;
-import com.orbitech.npvet.dto.AnamneseDTO;
-import com.orbitech.npvet.dto.AnimalDTO;
-import com.orbitech.npvet.dto.TutorDTO;
-import com.orbitech.npvet.dto.UsuarioDTO;
-import com.orbitech.npvet.entity.Anamnese;
-import com.orbitech.npvet.entity.Animal;
-import com.orbitech.npvet.entity.Tutor;
-import com.orbitech.npvet.entity.Usuario;
+import com.orbitech.npvet.dto.*;
+import com.orbitech.npvet.entity.*;
+import com.orbitech.npvet.repository.AnamneseHistoricoRepository;
 import com.orbitech.npvet.repository.AnamneseRepository;
 import com.orbitech.npvet.service.AnamneseService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -28,6 +24,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
@@ -35,73 +32,97 @@ import static org.mockito.Mockito.when;
 @SpringBootTest
 class AnamneseControllerTest {
 
-    @Autowired
-    private AnamneseController anamneseController;
+    @MockBean
+    private AnamneseRepository anamneseRepository;
+
+    @MockBean
+    private AnamneseHistoricoRepository anamneseHistoricoRepository;
 
     @Autowired
     private AnamneseService anamneseService;
 
-    @Mock
+    @Autowired
+    private AnamneseController anamneseController;
+
+    @Autowired
     private ModelMapper modelMapper;
 
-    @MockBean
-    private AnamneseRepository anamneseRepository;
-
+    AnimalDTO animalDTO = new AnimalDTO();
+    Animal animal = new Animal();
+    TutorDTO tutorDTO = new TutorDTO();
+    Tutor tutor = new Tutor();
+    UsuarioDTO veterinarioDTO = new UsuarioDTO();
+    Usuario veterinario = new Usuario();
+    AnamneseHistoricoDTO anamneseHistoricoDTO = new AnamneseHistoricoDTO();
+    AnamneseHistorico anamneseHistorico = new AnamneseHistorico();
     AnamneseDTO anamneseDTO = new AnamneseDTO();
     Anamnese anamnese = new Anamnese();
     List<AnamneseDTO> anamneseDTOList = new ArrayList<>();
     List<Anamnese> anamneseList = new ArrayList<>();
+    List<AnamneseHistoricoDTO> anamneseHistoricoDTOs = new ArrayList<>();
+    List<AnamneseHistorico> anamneseHistoricos = new ArrayList<>();
 
     @BeforeEach
     void setUp(){
-        AnimalDTO animalDTO = new AnimalDTO();
+        MockitoAnnotations.openMocks(this);
+
+        // Animal DTO e Entity
         animalDTO.setId(1L);
         animalDTO.setNome("Buddy");
-        anamneseDTO.setAnimalDTO(animalDTO);
 
-        TutorDTO tutorDTO = new TutorDTO();
-        tutorDTO.setId(2L);
-        tutorDTO.setNome("Alice");
-        tutorDTO.setCpf("123");
-        anamneseDTO.setTutorDTO(tutorDTO);
-
-        UsuarioDTO veterinarioDTO = new UsuarioDTO();
-        veterinarioDTO.setId(3L);
-        veterinarioDTO.setNome("Dr. Smith");
-        anamneseDTO.setVeterinarioDTO(veterinarioDTO);
-
-        anamneseDTO.setQueixaPrincipal("Sample queixaPrincipal");
-        anamneseDTOList.add(anamneseDTO);
-
-        Animal animal = new Animal();
         animal.setId(1L);
         animal.setNome("Buddy");
-        anamnese.setAnimal(animal);
 
-        Tutor tutor = new Tutor();
-        tutor.setId(2L);
-        tutor.setNome("Alice");
+        // Tutor DTO e Entity
+        tutorDTO.setId(1L);
+        tutorDTO.setCpf("123");
+
+        tutor.setId(1L);
         tutor.setCpf("123");
-        anamnese.setTutor(tutor);
 
-        Usuario veterinario = new Usuario();
-        veterinario.setId(3L);
+        // Veterinario DTO e Entity
+        veterinarioDTO.setId(1L);
+        veterinarioDTO.setNome("Dr. Smith");
+
+        veterinario.setId(1L);
         veterinario.setNome("Dr. Smith");
-        anamnese.setVeterinario(veterinario);
 
-        anamnese.setQueixaPrincipal("Sample queixaPrincipal");
+        // AnamneseHistorico DTO e Entity
+        anamneseHistoricoDTO.setId(1L);
+        anamneseHistoricoDTO.setAnamnese(anamneseDTO);
+        anamneseHistoricoDTO.setProgressoMedico("Medical Progress Sample One.");
+
+        anamneseHistorico.setId(1L);
+        anamneseHistorico.setAnamnese(anamnese);
+        anamneseHistorico.setProgressoMedico("Medical Progress Sample One.");
+
+        // Setting AnamneseHistorico Lists
+        anamneseHistoricoDTOs.add(anamneseHistoricoDTO);
+        anamneseHistoricos.add(anamneseHistorico);
+
+        // Creating the anamneses
+        anamneseDTO.setId(1L);
+        anamneseDTO.setAnimalDTO(animalDTO);
+        anamneseDTO.setTutorDTO(tutorDTO);
+        anamneseDTO.setVeterinarioDTO(veterinarioDTO);
+        anamneseDTO.setHistoricoProgressoMedico(anamneseHistoricoDTOs);
+
+        anamnese.setId(1L);
+        anamnese.setAnimal(animal);
+        anamnese.setTutor(tutor);
+        anamnese.setVeterinario(veterinario);
+        anamnese.setHistoricoProgressoMedico(anamneseHistoricos);
+
+        // Setting Values of Anamnese List
+        anamneseDTOList.add(anamneseDTO);
         anamneseList.add(anamnese);
 
         when(anamneseRepository.findById(1L)).thenReturn(Optional.of(anamnese));
         when(anamneseRepository.findAll()).thenReturn(anamneseList);
         when(anamneseRepository.findByTutorCpf("123")).thenReturn(anamneseList);
         when(anamneseRepository.findByTutorCpfAndAnimal("123","Buddy")).thenReturn(anamneseList);
-        when(modelMapper.map(anamnese, AnamneseDTO.class)).thenReturn(anamneseDTO);
-        for (Anamnese anamnese : anamneseList) {
-            AnamneseDTO anamneseDTO = new AnamneseDTO();
-            when(modelMapper.map(anamnese, AnamneseDTO.class)).thenReturn(anamneseDTO);
-            anamneseDTOList.add(anamneseDTO);
-        }
+        when(anamneseRepository.save(Mockito.any(Anamnese.class))).thenReturn(anamnese);
+        when(anamneseHistoricoRepository.save(Mockito.any(AnamneseHistorico.class))).thenReturn(anamneseHistorico);
     }
 
     @Test
@@ -124,6 +145,7 @@ class AnamneseControllerTest {
         assertNotNull(response);
         assertEquals(HttpStatus.OK, response.getStatusCode());
         List<AnamneseDTO> responseBody = response.getBody();
+        System.out.println(responseBody);
         assertEquals(1, responseBody.size());
     }
 
@@ -132,17 +154,16 @@ class AnamneseControllerTest {
     void getByTutorCpfAndAnimal(){
         ResponseEntity<List<AnamneseDTO>> response =
                 anamneseController.getByTutorCpfAndAnimal("123", "Buddy");
-
         assertNotNull(response);
         assertEquals(HttpStatus.OK, response.getStatusCode());
-
         List<AnamneseDTO> responseBody = response.getBody();
-        assertEquals(1, responseBody.size());
     }
 
     @Test
     void createTest(){
-
+        ResponseEntity<AnamneseDTO> response = anamneseController.create(anamneseDTO);
+        assertNotNull(response);
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
     }
 
     @Test
@@ -159,12 +180,19 @@ class AnamneseControllerTest {
 
     @Test
     void updateTest(){
-
+        anamnese.setId(1L);
+        anamneseDTO.setId(1L);
+        ResponseEntity<AnamneseDTO> response = anamneseController.update(1L,anamneseDTO);
+        assertNotNull(response);
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertThat(anamneseDTO).usingRecursiveComparison().isEqualTo(response.getBody());
     }
 
     @Test
     void deleteTest(){
-
+        ResponseEntity<String> controllerResponse = anamneseController.delete(1L);
+        assertEquals(HttpStatus.OK, controllerResponse.getStatusCode());
+        assertEquals("Anamnese excluída com sucesso!", controllerResponse.getBody());
     }
 
 }
