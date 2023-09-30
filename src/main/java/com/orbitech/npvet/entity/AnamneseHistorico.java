e1ff9646ae7c4e8a670f98f8e1577744d7293553
Bouchra Akl <bushraakl1234@gmail.com>
2023-09-30 10:55:50 -0300

    fix : Code Smells Resolved.

--- src/main/java/com/orbitech/npvet/entity/AnamneseHistorico.java
+++ src/main/java/com/orbitech/npvet/entity/AnamneseHistorico.java
@@ -34,7 +34,7 @@ public class AnamneseHistorico extends AbstractEntity{
     private LocalDate dataAtualizacao;
 
     @PrePersist
-    private void prePersist() {
+    private void prePersistDataAtualizacao() {
         this.dataAtualizacao = LocalDate.now();
     }
 
