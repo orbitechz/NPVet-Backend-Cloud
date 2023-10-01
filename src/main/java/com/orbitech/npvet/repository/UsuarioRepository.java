374157946c5c06fee9c580eb2c47d57fc796be11
vainicius <comercial.viniciusgabriel@gmail.com>
2023-10-01 17:49:46 -0300

    feat: Criando consultas em Usuario e Consulta com @Query.

--- src/main/java/com/orbitech/npvet/repository/UsuarioRepository.java
+++ src/main/java/com/orbitech/npvet/repository/UsuarioRepository.java
@@ -1,9 +1,30 @@
 package com.orbitech.npvet.repository;
 
+import com.orbitech.npvet.entity.TipoUsuario;
 import com.orbitech.npvet.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
+
+    @Query("SELECT c FROM Usuario c WHERE c.nome like :nome")
+    List<Usuario>findAllUsuariosByNome(@Param("nome")String nome);
+
+    @Query("SELECT c FROM Usuario c WHERE c.tipoUsuario = :tipoUsuario")
+    List<Usuario>findByTipoUsuario(@Param("tipoUsuario")TipoUsuario tipoUsuario);
+
+    @Query("SELECT c FROM Usuario c WHERE c.id = :id")
+    List<Usuario>findUsuarioById(@Param("id")Long id);
+
+    @Query("SELECT c FROM Usuario c WHERE c.username = :username")
+    List<Usuario>findUsuarioByUsername(@Param("username")String username);
+
+    @Query("SELECT c FROM Usuario c WHERE c.cpf = :cpf")
+    List<Usuario>findUsuarioByCpf(@Param("cpf")String cpf);
+
 }
