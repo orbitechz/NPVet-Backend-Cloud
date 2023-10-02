0275cbf3d2defd308ceab311e9483bcd32a3712e
Jean - Autofoz <jeanfelipe0610@gmail.com>
2023-10-02 19:06:43 -0300

    fix: fix nmr 3940892 da consulta

--- src/test/java/com/orbitech/npvet/ServiceTest/ConsultaServiceTest.java
+++ src/test/java/com/orbitech/npvet/ServiceTest/ConsultaServiceTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 import java.time.LocalDateTime;
 import java.time.Month;
