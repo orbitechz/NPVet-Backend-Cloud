c1c6d07e9a7018e6a733ebea960f8a91a3b358d9
vainicius <comercial.viniciusgabriel@gmail.com>
2023-10-02 00:09:25 -0300

    test: Consultas -> Novos testes finalizados.

--- src/test/java/com/orbitech/npvet/ControllerTest/ConsultaControllerTest.java
+++ src/test/java/com/orbitech/npvet/ControllerTest/ConsultaControllerTest.java
@@ -24,6 +24,7 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
@@ -87,6 +88,16 @@ class ConsultaControllerTest {
         when(repository.findAll()).thenReturn(consultaList);
         when(repository.save(Mockito.any(Consulta.class))).thenReturn(consultaEntidade);
 
+        when(repository.findConsultaByAnimalId(Mockito.anyLong())).thenReturn(consultaList);
+        when(repository.findConsultaByAnimalName(Mockito.anyString())).thenReturn(consultaList);
+        when(repository.findConsultaByVeterinarioNome(Mockito.anyString())).thenReturn(consultaList);
+        when(repository.findConsultaByVeterinarioId(Mockito.anyLong())).thenReturn(consultaList);
+        when(repository.findConsultaByAnamneseId(Mockito.anyLong())).thenReturn(consultaList);
+
+        when(repository.findConsultasByStatus(Status.EM_ANDAMENTO)).thenReturn(consultaList);
+        when(repository.findConsultasByStatus(Status.CONCLUIDA)).thenReturn(consultaList);
+        when(repository.findConsultasByStatus(Status.AGENDADA)).thenReturn(consultaList);
+        when(repository.findConsultasByStatus(Status.CANCELADA)).thenReturn(consultaList);
     }
 
     @Test
@@ -122,5 +133,73 @@ class ConsultaControllerTest {
         verify(repository,times(1)).deleteById(1L);
     }
 
+    @Test
+    void getConsultaByAnimalIdTest(){
+        Long id = 1L;
+        ResponseEntity<List<ConsultaDTO>>response = controller.getAnimalById(id);
+        assertEquals(HttpStatus.OK,response.getStatusCode());
+        assertThat(response.getBody()).usingRecursiveComparison().isEqualTo(consultaDTOList);
+    }
 
+    @Test
+    void getAnimalByNameTest(){
+        ResponseEntity<List<ConsultaDTO>>response = controller.getAnimalByName("nome");
+        assertEquals(HttpStatus.OK,response.getStatusCode());
+        assertThat(response.getBody()).usingRecursiveComparison().isEqualTo(consultaDTOList);
+    }
+
+    @Test
+    void getVeterinarioByNameTest(){
+        ResponseEntity<List<ConsultaDTO>>response = controller.getVeterinarioByNome("nome");
+        assertEquals(HttpStatus.OK,response.getStatusCode());
+        assertThat(response.getBody()).usingRecursiveComparison().isEqualTo(consultaDTOList);
+    }
+
+    @Test
+    void getConsultaByVeterinarioIdTest(){
+        Long id = 1L;
+        ResponseEntity<List<ConsultaDTO>>response = controller.getVeterinarioById(id);
+        assertEquals(HttpStatus.OK,response.getStatusCode());
+        assertThat(response.getBody()).usingRecursiveComparison().isEqualTo(consultaDTOList);
+    }
+
+    @Test
+    void getConsultaByAnamneseIdTest(){
+        Long id = 1L;
+        ResponseEntity<List<ConsultaDTO>>response = controller.getAnamneseById(id);
+        assertEquals(HttpStatus.OK,response.getStatusCode());
+        assertThat(response.getBody()).usingRecursiveComparison().isEqualTo(consultaDTOList);
+    }
+
+    @Test
+    void getConsultaEmAndamentoTest(){
+        ResponseEntity<List<ConsultaDTO>>response = controller.getConsultasEmAndamento();
+        assertNotNull(response);
+        assertEquals(HttpStatus.OK,response.getStatusCode());
+        assertThat(response.getBody().get(0)).usingRecursiveComparison().isEqualTo(consultaDTO);
+    }
+
+    @Test
+    void getConsultaConcluida(){
+        ResponseEntity<List<ConsultaDTO>>response = controller.getConsultasConcluida();
+        assertNotNull(response);
+        assertEquals(HttpStatus.OK,response.getStatusCode());
+        assertThat(response.getBody().get(0)).usingRecursiveComparison().isEqualTo(consultaDTO);
+    }
+
+    @Test
+    void getConsultaCanceladaTest(){
+        ResponseEntity<List<ConsultaDTO>>response = controller.getConsultasCancelada();
+        assertNotNull(response);
+        assertEquals(HttpStatus.OK,response.getStatusCode());
+        assertThat(response.getBody().get(0)).usingRecursiveComparison().isEqualTo(consultaDTO);
+    }
+
+    @Test
+    void getConsultaAgendadaTest(){
+        ResponseEntity<List<ConsultaDTO>>response = controller.getConsultasAgendadas();
+        assertNotNull(response);
+        assertEquals(HttpStatus.OK,response.getStatusCode());
+        assertThat(response.getBody().get(0)).usingRecursiveComparison().isEqualTo(consultaDTO);
+    }
 }
