f3c3dfa6fae93fb57418bcd7fdecb6dd827b1132
Jean Moschen - Pessoal <jeanfelipe0610@gmail.com>
2024-03-29 18:20:48 -0300

    [ Fix ]: All origins

--- src/main/java/com/orbitech/npvet/oauth/security/ApplicationSecurityConfig.java
+++ src/main/java/com/orbitech/npvet/oauth/security/ApplicationSecurityConfig.java
@@ -57,8 +57,8 @@ public class ApplicationSecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:4200");
-        config.setAllowedHeaders(Arrays.asList(HttpHeaders.AUTHORIZATION,HttpHeaders.CONTENT_TYPE,HttpHeaders.ACCEPT));
+        config.addAllowedOriginPattern("*");
+        config.setAllowedHeaders(Arrays.asList(HttpHeaders.AUTHORIZATION,HttpHeaders.CONTENT_TYPE,HttpHeaders.ACCEPT, HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN));
         config.setAllowedMethods(Arrays.asList(HttpMethod.GET.name(),HttpMethod.POST.name(),HttpMethod.PUT.name(),HttpMethod.DELETE.name()));
         config.setMaxAge(3600L);
         source.registerCorsConfiguration("/**", config);
