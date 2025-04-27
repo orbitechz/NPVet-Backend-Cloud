95440472bfa45807c0b2f04f714a80e45e465a8b
Vinicius <vinicius.gabriel@itaipuparquetec.org.br>
2025-04-27 19:40:59 -0300

    fix: Adicionando o @RestController

--- src/main/java/com/orbitech/npvet/controller/HealthController.java
+++ src/main/java/com/orbitech/npvet/controller/HealthController.java
@@ -4,7 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RestController;
+@RestController
 @RequestMapping("/npvet-api/health")
 public class HealthController {
 
